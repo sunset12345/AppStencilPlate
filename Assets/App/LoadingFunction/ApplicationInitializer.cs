d86265e506fa3da1013e60309201c450ac3f8ca1
崔铭玉 <cuimingyu@cuimingyudeMacBook-Pro.local>
2025-03-29 19:14:20 +0800

    初始功能补充

---
+++ Assets/App/LoadingFunction/ApplicationInitializer.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using DFDev.AssetBundles;
+using DFDev.UI.Layer;
+using UnityEngine;
+
+namespace App.LoadingFunction
+{
+    public class ApplicationInitializer : MonoBehaviour
+    {
+        [SerializeField] private string _assetBundleCode;
+        [SerializeField] private string _enterSceneName;
+
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+            Input.multiTouchEnabled = false;
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+#if ENABLE_GM
+            DG.Tweening.DOTween.Init(false, false, DG.Tweening.LogBehaviour.Default);
+            Debug.unityLogger.logEnabled = true;
+            OSDev.CSVConfig.ConfigBase.ErrorLogger = error => Debug.unityLogger.Log(LogType.Error, error);
+#endif
+            AssetBundleManager.Instance.Init(
+                AssetBundleUpdateProcessor.VersionFileRelativePath,
+                _assetBundleCode);
+
+            LayerManager.Instance.CreateLayer(
+                (int)LayerTag.Loading,
+                new DefaultLayerController());
+            // PushManager.Instance.TriggerTokenRequest();
+            LayerManager.Instance.AddLayerLoader((path, layer) =>
+            {
+                path.ResolveAssetPath(out var bundle, out var asset);
+                var prefab = AssetBundleManager.Instance.LoadAsset<GameObject>(bundle, asset);
+                Debug.Assert(prefab, $"add layer error:{path}");
+                var go = GameObject.Instantiate(
+                    prefab,
+                    layer.Root,
+                    false);
+                go.name = prefab.name;
+                return go;
+            });
+
+            LayerManager.Instance.LoadContent(
+                LayerTag.Loading,
+                "ui/loading/LoadingLayer");
+            ApplicationLoader.StartLoading(FinishLoading);
+        }
+
+        private async void FinishLoading()
+        {
+            // var userToken = UserDataManager.UserToken;
+            // if (string.IsNullOrEmpty(userToken))
+            //     UserDataManager.Instance.SetUserToken();
+            // AudioManager.PlayMusic(AudioName.MainBGM);
+            // await SceneManager.LoadSceneAsync(_enterSceneName);
+
+            // MainLayer.Create();
+            // GameLoginLayer.Create();
+            // LayerManager.Instance.GetLayerController((int)LayerTag.Loading)?.CloseAll();
+        }
+    }
+
+}
---
+++ Assets/App/LoadingFunction/LayerTag.cs
@@ -0,0 +1,16 @@
+namespace App.LoadingFunction
+{
+    public static class LayerTag
+    {
+        public const int Background = 0;
+        public const int GamePlay = 10;
+        public const int Main = 20;
+        public const int Dialog = 25;
+        public const int Popup = 27;
+        public const int Tip = 30;
+        public const int Guide = 32;
+        public const int Loading = 35;
+        public const int Transition = 38;
+        public const int Waiting = 40;
+    }
+}
