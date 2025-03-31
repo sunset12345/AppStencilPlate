92a92009d8d54b488862f94bb00fcaabb0278309
崔铭玉 <cuimingyu@cuimingyudeMacBook-Pro.local>
2025-03-31 15:22:24 +0800

    预制体调整

--- Assets/App/UI/Main/MainLayer.cs
+++ Assets/App/UI/Main/MainLayer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using App.LoadingFunction;
 using DFDev.EventSystem;
 using DFDev.UI.Layer;
@@ -9,13 +11,58 @@ namespace App.UI.Main
     {
         [SerializeField] private Transform navigatorRoot;
         [SerializeField] private Transform _panelRoot;
-
-
+        [SerializeField] private MainPanelType _defaultPanel = MainPanelType.Planet;
+    
+        private MainNavigateItem _currentNavigator;
+        public MainNavigateItem CurrentNavigator => _currentNavigator;
         private EventSubscriber _eventSubscriber;
+        private readonly Dictionary<MainNavigateItem, GameObject> _panelCache = new();
 
         void Awake()
         {
             _eventSubscriber = new EventSubscriber();
+            _eventSubscriber.Subscribe<MainNavigatorItemSelectedEvent>(OnMainNavigatorSelected);
+        }
+
+        void Start()
+        {
+            var navigators = navigatorRoot.GetComponentsInChildren<MainNavigateItem>();
+            var navigator = navigators.FirstOrDefault(item => item.Type == _defaultPanel);
+            if (navigator != null)
+            {
+                var toggle = navigator.Toggle;
+                if (toggle.isOn)
+                {
+                    toggle.onValueChanged.Invoke(true);
+                }
+                else
+                {
+                    toggle.isOn = true;
+                }
+            }
+        }
+
+        private void OnMainNavigatorSelected(MainNavigatorItemSelectedEvent evt)
+        {
+            if (_currentNavigator != null)
+            {
+                if (evt.Item == _currentNavigator) return;
+                var currentPanel = _panelCache[_currentNavigator];
+                currentPanel.SetActive(false);
+                _currentNavigator = null;
+            }
+
+            _currentNavigator = evt.Item;
+            if (!_panelCache.TryGetValue(_currentNavigator, out var panel))
+            {
+                panel = GameObject.Instantiate(
+                    _currentNavigator.PanelPrefab,
+                    _panelRoot,
+                    false);
+                _panelCache[_currentNavigator] = panel;
+            }
+
+            panel.SetActive(true);
         }
 
         void OnDestroy()
@@ -34,10 +81,8 @@ namespace App.UI.Main
     public enum MainPanelType
     {
         None = 0,
-        Main = 1,
-        Video = 2,
-        Match = 3,
-        Message = 4,
-        UserInfo = 5,
+        Planet = 1,
+        Message = 2,
+        UserInfo = 3,
     }
 }
